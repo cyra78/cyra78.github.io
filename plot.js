3b40abedf7cd168acd48188b401d7839169d0ce6
cyra78 <sairam.sadanandan@gmail.com>
2016-10-29 23:12:03 +0530

    Seperate legand introduced

--- plot.js
+++ plot.js
@@ -13,18 +13,19 @@ var yIncrement=cellRadius*2+cellPadding;
 function plot()
 {
     r = Raphael("chart", width, height);
-	r.rect(0,0,width,height).attr({fill: "#1a1a1a"})
+    r.rect(0,0,width,height).attr({fill: "#a8a8a8","stroke-width":0})
     var zeroLoc=cellRadius+cellPadding;
     var x=zeroLoc;
     var y=zeroLoc;
     var j=0;
-    for (var i = 0; i < plotData.length; i++)
+    var itemsCount=plotData.length;
+    for (var i = 0; i < itemsCount; i++)
     {
         var count=plotData[i].value;
         while(count>0)
         {
             r.circle(x,y,cellRadius).attr({fill: plotData[i].color, "stroke-width":0});
-            count /=cellScale;
+            count -=cellScale;
             x+=xIncrement;
             j++;
             if(j>cellsPerRow)
@@ -35,20 +36,38 @@ function plot()
             }
         }
     }
-	x=zeroLoc;
-	y+=yIncrement;
-	var fontSz=16;
-	var xPadding=5;
-	var yPadding=10;
-	var rWidth=75;
-	var rHeight=fontSz+yPadding;
-	var rectRounding=7;
-	var labels = r.set();
+    
+    //plot legand
+    x=zeroLoc;
+    y=zeroLoc;
+    var fontSz=16;
+    var xPadding=5;
+    var yPadding=10;
+    var rWidth=75;
+    var rHeight=fontSz+yPadding/2;
+    var rectRounding=7;
+    var labels = r.set();
+    
+    //recalculate height based on overflow of items
+    var legandRowCount=1;
+    if(itemsCount*rWidth > width)
+    {
+        legandRowCount=(itemsCount*rWidth)/width;
+    }
+    height=legandRowCount*(rHeight+yPadding);
+    
+    r = Raphael("legend", width, height);
+    r.rect(0,0,width,height).attr({fill: "#fff","stroke-width":0})
     for (i = 0; i < plotData.length; i++)
     {
-		r.rect(x,y,rWidth,rHeight).attr({fill:plotData[i].color,r:rectRounding});
-		labels.push(r.text(x+xPadding,y+yPadding,plotData[i].name));
-		y+=26;
+        r.rect(x,y,rWidth,rHeight).attr({fill:plotData[i].color,r:rectRounding,"stroke-width":0});
+        labels.push(r.text(x+xPadding,y+yPadding,plotData[i].name));
+        x+=rWidth;
+        if(x+rWidth>width)
+        {
+            x=zeroLoc;
+            y+=rHeight;
+        }
     }labels.attr({font: "16px Fontin-Sans, Arial", fill: "#fff", "text-anchor": "start"});
 }
 
@@ -62,6 +81,9 @@ function load() {
     cellRadius=parseInt(jsonData.cellRadius);
     xIncrement=cellRadius*2+cellPadding;
     yIncrement=cellRadius*2+cellPadding;
+    
+    if(xIncrement*cellsPerRow>width)
+        width=xIncrement*cellsPerRow;
     var graphData=jsonData.data;
     for (var i = 0; i < graphData.length; i++)
     {
